99c7f50a8049a5fc85e886e83ec31e06c006a019
fabricastro <fabricastrook@gmail.com>
2024-12-29 23:56:40 -0300

    cambio de imagenes ciclos

--- src/components/CardsCiclos.jsx
+++ src/components/CardsCiclos.jsx
@@ -46,12 +46,13 @@ const CardCiclos = () => {
                         {group.map((card) => (
                             <div
                                 key={card.id}
-                                className="flex flex-col border border-gray-300 overflow-hidden shadow-md min-h-[700px]"
+                                className="flex flex-col border border-gray-300 overflow-hidden shadow-md min-h-[750px]"
                                 style={{ backgroundColor: card.bgColor, color: card.textColor }}>
                                 <img src={card.imgSrc} alt={card.title} className="min-h-[280px] w-full object-cover" />
-                                <div className="p-4 flex flex-col justify-between flex-1">
-                                    <div>
-                                        <h3 className="text-[28px] text-center uppercase">{card.title}</h3>
+                                <div className="p-4 flex flex-col justify-between flex-1 py-6 gap-6">
+                                    <div className='items-center flex flex-col'>
+                                        <img src={card.iconSrc} alt='estrella' className='max-h-[30px] mb-6' />
+                                        <h3 className="text-[28px] text-center uppercase tracking-widest">{card.title}</h3>
                                         <div
                                             className="text-[20px] font-textos text-center mt-2"
                                             dangerouslySetInnerHTML={{ __html: card.shortDescription }}
@@ -100,48 +101,65 @@ const CardCiclos = () => {
                         }`}
                 >
                     <div
-                        className={`p-4 md:p-6 shadow-lg w-full h-[90vh] md:h-auto overflow-auto max-w-xs sm:max-w-md lg:max-w-3xl relative transform transition-transform duration-300 ${isVisible && !isExiting ? 'scale-100 opacity-100' : 'scale-90 opacity-0'
+                        className={`md:p-0 shadow-lg w-full h-[90vh] md:h-auto overflow-auto max-w-xs sm:max-w-md lg:max-w-3xl relative transform transition-transform duration-300 ${isVisible && !isExiting ? 'scale-100 opacity-100' : 'scale-90 opacity-0'
                             }`}
                         style={{ backgroundColor: selectedCard.bgColor }}
                     >
-                        {/* Botón de cerrar */}
-                        <button
-                            className="absolute top-2 right-2 sm:top-4 sm:right-4 text-lg font-bold"
-                            style={{ color: selectedCard.textColor }}
-                            onClick={closeModal}>
-                            &times;
-                        </button>
-                        <div style={{ color: selectedCard.textColor }}>
-                            {/* Contenido del encabezado */}
-                            <h4 className="font-semibold text-lg sm:text-xl">{selectedCard.popupContent.header}</h4>
-                            <p className="font-textos text-sm sm:text-md">{selectedCard.popupContent.intro}</p>
-                            <h5 className="font-semibold text-lg sm:text-xl">{selectedCard.popupContent.question}</h5>
+                        <div className="grid grid-cols-1 md:grid-cols-[1fr_2fr] gap-4 h-full">
+                            {/* Imagen a la izquierda */}
+                            <div className="relative w-full h-full">
+                                <img
+                                    src={selectedCard.popupContent.img}
+                                    alt="Imagen del popup"
+                                    className="w-full h-full object-cover"
+                                />
+                            </div>
 
-                            {/* Descripción HTML */}
-                            <div
-                                className="font-textos text-sm sm:text-md"
-                                dangerouslySetInnerHTML={{ __html: selectedCard.popupContent.description }}
-                            />
+                            {/* Contenido del popup */}
+                            <div className="relative p-4 flex flex-col justify-between">
+                                {/* Botón de cerrar */}
+                                <button
+                                    className="absolute top-2 right-2 sm:top-4 sm:right-4 text-lg font-bold"
+                                    style={{ color: selectedCard.textColor }}
+                                    onClick={closeModal}
+                                >
+                                    &times;
+                                </button>
 
-                            {/* Elementos en columnas */}
-                            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4 font-textos">
-                                {selectedCard.popupContent.items.map((item, index) => (
+                                <div style={{ color: selectedCard.textColor }}>
+                                    {/* Contenido del encabezado */}
+                                    <h4 className="font-semibold text-lg sm:text-xl">{selectedCard.popupContent.header}</h4>
+                                    <p className="font-textos text-sm sm:text-md">{selectedCard.popupContent.intro}</p>
+                                    <h5 className="font-semibold text-lg sm:text-xl">{selectedCard.popupContent.question}</h5>
+
+                                    {/* Descripción HTML */}
                                     <div
-                                        key={index}
-                                        className="border-l-4 pl-4"
-                                        style={{ borderColor: selectedCard.textColor }}
-                                    >
-                                        <h4 className="text-lg sm:text-[28px]">{item.number}</h4>
-                                        <p className="font-textos text-sm sm:text-md">
-                                            <strong>{item.title}</strong> {item.content}
-                                        </p>
+                                        className="font-textos text-sm sm:text-md"
+                                        dangerouslySetInnerHTML={{ __html: selectedCard.popupContent.description }}
+                                    />
+
+                                    {/* Elementos en columnas */}
+                                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4 font-textos">
+                                        {selectedCard.popupContent.items.map((item, index) => (
+                                            <div
+                                                key={index}
+                                                className="border-l-4 pl-4"
+                                                style={{ borderColor: selectedCard.textColor }}
+                                            >
+                                                <h4 className="text-lg sm:text-[28px]">{item.number}</h4>
+                                                <p className="font-textos text-sm sm:text-md">
+                                                    <strong>{item.title}</strong> {item.content}
+                                                </p>
+                                            </div>
+                                        ))}
                                     </div>
-                                ))}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             )}
+
         </section>
     );
 };
